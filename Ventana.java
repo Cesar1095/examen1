8b503533f19941f1a3d469b7b9e7b7f1d1db79d8
Cesar Espino <ceraespinose@ittepic.edu.mx>
2016-02-22 00:40:04 -0700

    Creando main

--- Ventana.java
+++ Ventana.java
@@ -7,11 +7,16 @@ public class Ventana extends JFrame{
 
 	public Ventana(){
 		this.setTitle("prueba para examen");
-		setDefaultClose(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLayout(null);
 		btnBoton=new JButton("Nuevo boton");
 		btnBoton.setBounds(120,100,120,70);
 		add(btnBoton);		
-	
+		
+	}
+	public static void main(String ar[]){
+	Ventana V =new Ventana();
+	V.setBounds(0,0,300,200);
+	V.setVisible(true);
 	}
 }
