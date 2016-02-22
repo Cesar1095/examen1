14c5ec87e056e2d29c70da302a95a1cab610c206
Cesar Espino <ceraespinose@ittepic.edu.mx>
2016-02-21 22:54:31 -0700

    Iniciando la ventana

---
+++ Ventana.java
@@ -0,0 +1,16 @@
+
+import javax.swing.*;
+
+public class Ventana extends JFrame{
+	
+	private JButton btnBoton;
+
+	public Ventana(){
+		this.setTitle("prueba para examen");
+		setDefaultClose(JFrame.EXIT_ON_CLOSE);
+		setLayout(null);
+		btnBoton=new JButton("Nuevo boton");
+		
+	
+	}
+}
